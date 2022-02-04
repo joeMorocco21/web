974b52b6b545878908760baca6a5a097fe0b3c49
joeMorocco21 <68898597+joeMorocco21@users.noreply.github.com>
2022-02-04 10:08:53 +0100

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "mssql",
-        "NAME": "bdd",
-        "USER": "adminyoussef",
-        "PASSWORD": "641979Ab",
-        "HOST": "sqlyoussef.database.windows.net",
-        "PORT": "1433",
-        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server", 
+        "ENGINE": "***",
+        "NAME": "***",
+        "USER": "***",
+        "PASSWORD": "***",
+        "HOST": "***",
+        "PORT": "***",
+        "OPTIONS": {"driver": "***", 
         },
     }
 }
